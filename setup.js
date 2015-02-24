70e8c36b95965be1ca4c5e8d30cd9fa69841e091
Scott Gerike <scott.gerike@kineticdata.com>
2015-02-24 17:43:09 -0600

    Moved all of the navigation.js logic into setup.js and am now dynamically setting the height/width of the tiles using the returned grid layout

--- setup.js
+++ setup.js
@@ -2,11 +2,17 @@ var jsoninput = '{"amount": 6,"pluginsLocation" : "plugins","plugins" : {"youtub
 var tileCount;
 var pluginsLocation;
 
+// Document.ready function
 $(function() {
+  // Load plugin file
   var json = JSON.parse(jsoninput);
-  var counter = 0;
   tileCount = json.amount;
   pluginsLocation = json.pluginsLocation;
+
+  /***************************************************************************
+                          CREATE THE MEDIA TILES
+  ****************************************************************************/
+  var counter = 0;
   for (var key in json.plugins) {
     if (json.plugins.hasOwnProperty(key)) {
       jQuery('<div/>', {
@@ -22,10 +28,88 @@ $(function() {
         'data-adaptive-background': '1'
       }).appendTo('#'+counter);
       counter += 1;
-      //console.log(json.plugins[key]);
     }
   }
 
   $("#0").addClass("selected");
   $.adaptiveBackground.run();
+
+  /* Add the CSS needed to make the tiles fit on one page with no scrolling */
+
+  // Get the grid dimensions
+  var dimensions = getGridDimensions(tileCount);
+  // If the dimensions did not return a usable grid, try again
+  var attempts = 1;
+  while (dimensions[0] === 0) { // if the grid width is 0, no grid layouts were found
+    dimensions = getGridDimensions(tileCount + attempts);
+    attempts += 1;
+  }
+  var gridWidth = dimensions[0];
+  var gridHeight = dimensions[1];
+
+  var tileHeight = 100/gridHeight;
+  var tileWidth = 100/gridWidth;
+  $(".img-wrapper").css({"height" : tileHeight+"%", "width" : tileWidth+"%"});
+
+  /***************************************************************************
+                       ACTIVATE KEYBOARD NAVIGATION
+  ****************************************************************************/
+  $("body").keydown(function(e) {
+    // If any of the directional keys are pressed
+    if ($.inArray(e.keyCode,[13,37,38,39,40]) != -1) {
+      var currentSelected = $(".selected")[0];
+      var currentId = parseInt(currentSelected.id);
+      var currentRow = Math.floor((currentId)/gridWidth);
+    }
+
+    if(e.keyCode == 37) { // left
+      var newId = (((currentId - 1) + tileCount) % gridWidth) + currentRow * gridWidth;
+      $(".selected").removeClass("selected");
+      $("#"+newId.toString()).addClass("selected");
+    }
+    else if (e.keyCode == 38) { // up
+      var newId = ((currentId - (gridWidth)) + tileCount) % tileCount;
+      $(".selected").removeClass("selected");
+      $("#"+newId.toString()).addClass("selected");
+    }
+    else if(e.keyCode == 39) { // right
+      var newId = (((currentId + 1) + tileCount) % gridWidth) + currentRow * gridWidth;
+      $(".selected").removeClass("selected");
+      $("#"+newId.toString()).addClass("selected");
+    }
+    else if(e.keyCode == 40) { // down
+      var newId = ((currentId + (gridWidth)) + tileCount) % tileCount;
+      $(".selected").removeClass("selected");
+      $("#"+newId.toString()).addClass("selected");
+    }
+    // If it is the enter key that is pressed
+    else if (e.keyCode == 13) {
+      console.log(currentSelected.title);
+      // Create the link from the div to go to the next grid pattern
+    }
+  });
 });
+
+
+function getGridDimensions(tiles) {
+    var width = 0;
+    var height = 0;
+    for (i = 2; i < tiles; i++) {
+      var possDims = {};
+      var division = (tiles/i).toString();
+      if (division.indexOf(".") == -1) {
+        possDims[division] = i;
+      }
+
+      for (var key in possDims) {
+        if (possDims.hasOwnProperty(key)) {
+          if (width == 0 || key > width || 
+            Math.abs(width-height) > Math.abs(key-possDims[key])) {
+            width = key;
+            height = possDims[key];
+          }
+        }
+      }
+    }
+  return [parseInt(width), parseInt(height)];
+}
