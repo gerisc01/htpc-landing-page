50f2d05f3a22a29e9580850af284283647625fbd
Scott Gerike <scott.gerike@kineticdata.com>
2015-02-26 16:10:28 -0600

    Now loading the real config files (instead of hardcoded), current location is modified in the navbar, and can now go forward and backward between levels

--- setup.js
+++ setup.js
@@ -1,114 +1,129 @@
-var jsoninput = '{ "tiles" : { "youtube" : { "id" : "youtube", "icon" : "images/youtube.jpg", "layout" : "home" }, "twitch" : { "id" : "twitch", "icon" : "images/twitch.jpeg", "layout" : "home" }, "pocketcasts" : { "id" : "pocketcasts", "icon" : "images/pocketcasts.png", "layout" : "home" }, "mlb-at-bat" : { "id" : "mlb-at-bat", "icon" : "images/mlb-at-bat.png", "layout" : "home" }, "mls-live" : { "id" : "mls-live", "icon" : "images/mls-live.png", "layout" : "home" }, "nbc-sports" : { "id" : "nbc-sports", "icon" : "images/nbc-sports.jpg", "layout" : "home" } }, "layouts" : { "home" : ["youtube","twitch","pocketcasts","mlb-at-bat","mls-live","nbc-sports"] } }';
+var jsoninput = '{ "tiles" : { "youtube" : { "title" : "YouTube", "id" : "youtube", "icon" : "images/youtube.jpg", "layout" : "home" }, "twitch" : { "title" : "Twitch", "id" : "twitch", "icon" : "images/twitch.jpeg", "layout" : "home" }, "pocketcasts" : { "title" : "PocketCasts", "id" : "pocketcasts", "icon" : "images/pocketcasts.png", "layout" : "home" }, "mlb-at-bat" : { "title" : "MLB At Bat", "id" : "mlb-at-bat", "icon" : "images/mlb-at-bat.png", "layout" : "home" }, "mls-live" : { "title" : "MLS Live", "id" : "mls-live", "icon" : "images/mls-live.png", "layout" : "home" }, "nbc-sports" : { "title" : "NBC Sports", "id" : "nbc-sports", "icon" : "images/nbc-sports.jpg", "layout" : "home" } }, "layouts" : { "home" : ["youtube","twitch","pocketcasts","mlb-at-bat","mls-live","nbc-sports"] } }';
 var youtubeinput = '{ "tiles" : { "subscribers" : { "title" : "Subscriptions", "id": "", "icon": "images/subscriptions.png", "layout": "subscribers" }, "playlists" : { "title" : "Playlists", "id": "", "icon": "images/playlists.png", "layout": "playlists" }, "watchlater" : { "title" : "Watch Later", "id": "", "icon": "images/watchlater.png", "layout": "watchlater" }, "popular" :{ "title" : "Popular", "id": "", "icon": "images/popular.jpg", "layout": "popular" }, "subscribers-list" : "getSubs()", "playlists-list" : "getPlaylists()", "videos-by-channel" : "getVideosByChannel()", "videos-by-playlist" : "getVideosByPlaylist()", "watchlater-videos" : "getWatchLaterVideos()", "popular-videos" : "getPopularVideos()" }, "layouts" : { "home" : ["subscribers","playlists","watchlater","popular"], "subscribers" : "subscribers-list", "playlists" : "playlists-list", "watchlater" : "watchlater-videos", "popular" : "popular-videos", "subscribers-list" : "videos-by-channel", "playlists-list" : "videos-by-playlist" } }';
 var tileCount = 0;
 var currentPlugin = null;
 
 // Document.ready function
 $(function() {
-  setupGrid(jsoninput, "home");
+  setupGrid("config.json", "home");
 });
 
 function setupGrid(config, layout) {
   // Load plugin file
-  var json = JSON.parse(config);
+  $.getJSON(config, function(json) {
+    /***************************************************************************
+                            CREATE THE MEDIA TILES
+    ****************************************************************************/
+    var keys = json.layouts[layout];
+    for (var i = 0; i < keys.length; i++) {
+      var key = keys[i];
+      jQuery('<div/>', {
+            id: tileCount,
+            class: "img-wrapper",
+            title: key
+        }).appendTo('#load-container');
 
-  /***************************************************************************
-                          CREATE THE MEDIA TILES
-  ****************************************************************************/
-  var keys = json.layouts[layout];
-  for (var i = 0; i < keys.length; i++) {
-    var key = keys[i];
-    jQuery('<div/>', {
-          id: tileCount,
-          class: "img-wrapper",
-          title: key
-      }).appendTo('#load-container');
+        var imgFile;
+        if (currentPlugin === null) {
+          imgFile = "plugins/" + key + "/" + json.tiles[key].icon;
+        } else {
+          imgFile = "plugins/" + currentPlugin + "/" + json.tiles[key].icon;
+        }
+        jQuery('<img/>', {
+          class: "tile",
+          src: imgFile,
+          'data-adaptive-background': '1'
+        }).appendTo('#'+tileCount);
 
-      var imgFile;
-      if (currentPlugin === null) {
-        imgFile = "plugins/" + key + "/" + json.tiles[key].icon;
-      } else {
-        imgFile = "plugins/" + currentPlugin + "/" + json.tiles[key].icon;
+        jQuery('<input/>', {
+          id: "layout",
+          type: "hidden",
+          value: json.tiles[key].layout
+        }).appendTo('#'+tileCount);
+
+        tileCount += 1;
+    }
+
+    $("#0").addClass("selected");
+    $.adaptiveBackground.run();
+
+    /* Add the CSS needed to make the tiles fit on one page with no scrolling */
+
+    // Get the grid dimensions
+    var dimensions = getGridDimensions(tileCount);
+    // If the dimensions did not return a usable grid, try again
+    var attempts = 1;
+    while (dimensions[0] === 0) { // if the grid width is 0, no grid layouts were found
+      dimensions = getGridDimensions(tileCount + attempts);
+      attempts += 1;
+    }
+    var gridWidth = dimensions[0];
+    var gridHeight = dimensions[1];
+
+    var tileHeight = 100/gridHeight;
+    var tileWidth = 100/gridWidth;
+    $(".img-wrapper").css({"height" : tileHeight+"%", "width" : tileWidth+"%"});
+
+    /***************************************************************************
+                         ACTIVATE KEYBOARD NAVIGATION
+    ****************************************************************************/
+    $("body").keydown(function(e) {
+      var currentSelected;
+      var currentId;
+      var currentRow;
+      var newId;
+      // If any of the directional keys are pressed
+      if ($.inArray(e.keyCode,[8,13,37,38,39,40]) != -1) {
+        currentSelected = $(".selected")[0];
+        currentId = parseInt(currentSelected.id);
+        currentRow = Math.floor((currentId)/gridWidth);
       }
-      jQuery('<img/>', {
-        class: "tile",
-        src: imgFile,
-        'data-adaptive-background': '1'
-      }).appendTo('#'+tileCount);
 
-      jQuery('<input/>', {
-        id: "layout",
-        type: "hidden",
-        text: json.tiles[key].layout
-      }).appendTo('#'+tileCount);
+      if(e.keyCode == 37) { // left
+        newId = (((currentId - 1) + tileCount) % gridWidth) + currentRow * gridWidth;
+        $(".selected").removeClass("selected");
+        $("#"+newId.toString()).addClass("selected");
+      }
+      else if (e.keyCode == 38) { // up
+        newId = ((currentId - (gridWidth)) + tileCount) % tileCount;
+        $(".selected").removeClass("selected");
+        $("#"+newId.toString()).addClass("selected");
+      }
+      else if(e.keyCode == 39) { // right
+        newId = (((currentId + 1) + tileCount) % gridWidth) + currentRow * gridWidth;
+        $(".selected").removeClass("selected");
+        $("#"+newId.toString()).addClass("selected");
+      }
+      else if(e.keyCode == 40) { // down
+        newId = ((currentId + (gridWidth)) + tileCount) % tileCount;
+        $(".selected").removeClass("selected");
+        $("#"+newId.toString()).addClass("selected");
+      }
+      else if (e.keyCode == 13) { // Enter
+        if (currentPlugin === null) {currentPlugin = currentSelected.title;}
+        var configLocation = currentPlugin === null ? "config.json" : "plugins/" + currentPlugin + "/config.json";
+        var nextLayout = $(".selected #layout").val();
+        clearLayout();
+        addToNavbar(json.tiles[currentPlugin].title, currentPlugin, nextLayout);
+        setupGrid(configLocation, nextLayout);
+        //setupGrid(youtubeinput, nextLayout);
+      }
+      else if (e.keyCode == 8 && !$(e.target).is("input, textarea")) {
+          e.preventDefault();
+          if ($("ul.nav li").size() > 1) {
+            var prevLayout = $("ul.nav li").eq(-2).children("#layout").val();
+            if ($("ul.nav li").eq(-2).children("#plugin").val() === "") {
+              currentPlugin = null;
+            }
 
-      tileCount += 1;
-  }
-
-  $("#0").addClass("selected");
-  $.adaptiveBackground.run();
-
-  /* Add the CSS needed to make the tiles fit on one page with no scrolling */
-
-  // Get the grid dimensions
-  var dimensions = getGridDimensions(tileCount);
-  // If the dimensions did not return a usable grid, try again
-  var attempts = 1;
-  while (dimensions[0] === 0) { // if the grid width is 0, no grid layouts were found
-    dimensions = getGridDimensions(tileCount + attempts);
-    attempts += 1;
-  }
-  var gridWidth = dimensions[0];
-  var gridHeight = dimensions[1];
-
-  var tileHeight = 100/gridHeight;
-  var tileWidth = 100/gridWidth;
-  $(".img-wrapper").css({"height" : tileHeight+"%", "width" : tileWidth+"%"});
-
-  /***************************************************************************
-                       ACTIVATE KEYBOARD NAVIGATION
-  ****************************************************************************/
-  $("body").keydown(function(e) {
-    // If any of the directional keys are pressed
-    if ($.inArray(e.keyCode,[13,37,38,39,40]) != -1) {
-      var currentSelected = $(".selected")[0];
-      var currentId = parseInt(currentSelected.id);
-      var currentRow = Math.floor((currentId)/gridWidth);
-    }
-
-    if(e.keyCode == 37) { // left
-      var newId = (((currentId - 1) + tileCount) % gridWidth) + currentRow * gridWidth;
-      $(".selected").removeClass("selected");
-      $("#"+newId.toString()).addClass("selected");
-    }
-    else if (e.keyCode == 38) { // up
-      var newId = ((currentId - (gridWidth)) + tileCount) % tileCount;
-      $(".selected").removeClass("selected");
-      $("#"+newId.toString()).addClass("selected");
-    }
-    else if(e.keyCode == 39) { // right
-      var newId = (((currentId + 1) + tileCount) % gridWidth) + currentRow * gridWidth;
-      $(".selected").removeClass("selected");
-      $("#"+newId.toString()).addClass("selected");
-    }
-    else if(e.keyCode == 40) { // down
-      var newId = ((currentId + (gridWidth)) + tileCount) % tileCount;
-      $(".selected").removeClass("selected");
-      $("#"+newId.toString()).addClass("selected");
-    }
-    // If it is the enter key that is pressed
-    else if (e.keyCode == 13) {
-      if (currentPlugin === null) {currentPlugin = currentSelected.title;}
-      console.log(currentSelected.title);
-      // var configLocation = "plugins/" + currentPlugin + "/config.json"
-      // $.getJSON(configLocation, function(json) {         
-      //   var nextLayout = ;
-      // });
-      var nextLayout = $(".selected #layout").text();
-      clearLayout();
-      setupGrid(youtubeinput, nextLayout);
-      // Create the link from the div to go to the next grid pattern
-    }
+            var configLocation = currentPlugin === null ? "config.json" : "plugins/" + currentPlugin + "/config.json";
+            clearLayout();
+            setupGrid(configLocation, prevLayout);
+            $("ul.nav li").eq(-1).remove();
+            $("ul.nav li").eq(-1).addClass("active");
+          }
+      }
+    });
   });
 }
 
@@ -116,6 +131,31 @@ function clearLayout() {
   tileCount = 0;
   $("#load-container").empty();
   $("body").off("keydown");
+}
+
+function addToNavbar(title, plugin, layout) {
+  $("ul.nav li").eq(-1).removeClass("active");
+  
+  jQuery("<li/>", {
+    class: "active"
+  }).appendTo("ul.nav");
+
+  jQuery("<a/>", {
+    href: "#",
+    text: title
+  }).appendTo("ul.nav li.active");
+
+  jQuery("<input/>", {
+    id: "plugin",
+    type: "hidden",
+    value: plugin
+  }).appendTo("ul.nav li.active");
+
+  jQuery("<input/>", {
+    id: "layout",
+    type: "hidden",
+    value: layout
+  }).appendTo("ul.nav li.active");
 }
 
 function getGridDimensions(tiles) {
@@ -130,7 +170,7 @@ function getGridDimensions(tiles) {
 
       for (var key in possDims) {
         if (possDims.hasOwnProperty(key)) {
-          if (width == 0 || key > width || 
+          if (width === 0 || key > width ||
             Math.abs(width-height) > Math.abs(key-possDims[key])) {
             width = key;
             height = possDims[key];
