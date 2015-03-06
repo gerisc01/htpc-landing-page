4671e55c09aa01259ccbc6f8678e3a9d3fc4720d
Scott Gerike <scottgerike@scott-air.local>
2015-03-06 01:01:51 -0600

    Returning videos for both following and top videos for Twitch plugin.

--- public/plugins/twitch/config.rb
+++ public/plugins/twitch/config.rb
@@ -59,7 +59,7 @@ class Twitch
             params["limit"] = pageSize - 1
             params["offset"] = pageSize - 1
             params["last_page"] = ((params["count"].to_i - (pageSize-1)*2)/(pageSize-2)) + 3;
-            following.slice(0,params["limit"].to_i)
+            following = following.slice(0,params["limit"].to_i)
         end
 
         # Find the total amount of pages possible based on count and limit. Also
@@ -116,7 +116,260 @@ class Twitch
         return data.to_json
     end
 
+    def getChannelVideos(params)
+        #== Params
+        # *id*: the 
+        # *limit*: the limit for the amount of tiles that will be on the page
+        # offset: the offset so that the results are started at the correct page
+        # count: the total count of the resource
+        # page: 1,2,3,4,...
+
+        req_params = {}
+        req_params["limit"] = params["limit"]
+        req_params["offset"] = params["offset"]
+        req_params["broadcasts"] = "true"
+
+        resp = RestClient.get("https://api.twitch.tv/kraken/channels/#{params['id']}/videos",:params => req_params, :authorization => "OAuth #{@access_token}")
+        json = JSON.parse(resp)
+
+        videos = json['videos']
+
+        # + If the the total count wasn't passed, find it from the response. 
+        # + Find the ideal pageSize using the getPageSize method. 
+        # + pageSize != limit, reset the limit to pageSize and call the method again
+        if params["count"].to_s.empty?
+            params["count"] = json["_total"]
+            params["page"] = "1"
+            pageSize = getPageSize(params["count"].to_i, params["limit"].to_i)
+            params["limit"] = pageSize - 1
+            params["offset"] = 0
+            params["last_page"] = ((params["count"].to_i - (pageSize-1)*2)/(pageSize-2)) + 3;
+            videos = videos.slice!(0,params["limit"].to_i)
+        end
+
+        # Find the total amount of pages possible based on count and limit. Also
+        # caulculate offset
+        last_page = params["last_page"].to_i
+        offset = (params["limit"].to_i * (params["page"].to_i - 1)) + (1 * ((params["page"].to_i-1)/1000)).ceil
+
+        # Configure the data
+        # + If first page, put in data for nextPage tile
+        # + If last page, put in data for prevPage tile
+        # + If middle page, put in data for both tiles
+        data = []
+        if params["page"] != "1"
+            if params["page"].to_i - 1 == 1 && params["last_page"] != "2"
+                limit = params['limit'].to_i + 1
+            elsif params["page"].to_i == params["last_page"]
+                limit = params['limit'].to_i - 1
+            else
+                limit = params['limit']
+            end
+            prev_page = {}
+            prev_page["title"] = "Previous"
+            prev_page["id"] = ""
+            prev_page["icon"] = "/prevPage.gif"
+            prev_page["layout"] = "/twitch/getChannelVideos?limit=#{limit}&count=#{params['count']}&page=#{params['page'].to_i-1}&id=#{params['id']}"
+            prev_page["layout"] += "&offset=#{params['offset'].to_i-params['limit'].to_i}&last_page=#{last_page}" # adding in the last_page variable as a temporary solution
+            data.push(prev_page) 
+        end
+        for video in videos
+            sub_data = {}
+            sub_data["title"] = video["title"]
+            sub_data["id"] = video["url"]
+            sub_data["icon"] = video["preview"]
+            sub_data["layout"] = ""
+            data.push(sub_data)
+        end
+        if params["page"] != last_page.to_s
+            if params["page"].to_i + 1 == last_page && params["page"] != "1"
+                limit = params['limit'].to_i + 1
+            elsif params["page"] == "1"
+                limit = params['limit'].to_i - 1
+            else
+                limit = params['limit']
+            end
+            next_page = {}
+            next_page["title"] = "Next"
+            next_page["id"] = ""
+            next_page["icon"] = "/nextPage.gif"
+            next_page["layout"] = "/twitch/getChannelVideos?limit=#{limit}&count=#{params['count']}&page=#{params['page'].to_i+1}&id=#{params['id']}" 
+            next_page["layout"] += "&offset=#{params['offset'].to_i+params['limit'].to_i}&last_page=#{last_page}" # adding in the last_page variable as a temporary solution
+            data.push(next_page)
+        end
+
+        return data.to_json
+    end
+
+    def getTopGames(params)
+        #== Params
+        # *limit*: the limit for the amount of tiles that will be on the page
+        # offset: the offset so that the results are started at the correct page
+        # count: the total count of the resource
+        # page: 1,2,3,4,...
+
+        req_params = {}
+        req_params["limit"] = params["limit"]
+        req_params["offset"] = params["offset"]
+
+        resp = RestClient.get("https://api.twitch.tv/kraken/games/top",:params => req_params, :authorization => "OAuth #{@access_token}")
+        json = JSON.parse(resp)
+
+        top_games = json['top']
+
+        # + If the the total count wasn't passed, find it from the response. 
+        # + Find the ideal pageSize using the getPageSize method. 
+        # + pageSize != limit, reset the limit to pageSize and call the method again
+        if params["count"].to_s.empty?
+            params["count"] = json["_total"]
+            params["page"] = "1"
+            pageSize = getPageSize(params["count"].to_i, params["limit"].to_i)
+            params["limit"] = pageSize - 1
+            params["offset"] = pageSize - 1
+            params["last_page"] = ((params["count"].to_i - (pageSize-1)*2)/(pageSize-2)) + 3;
+            top_games = top_games.slice(0,params["limit"].to_i)
+        end
+
+        # Find the total amount of pages possible based on count and limit. Also
+        # caulculate offset
+        last_page = params["last_page"].to_i
+        offset = (params["limit"].to_i * (params["page"].to_i - 1)) + (1 * ((params["page"].to_i-1)/1000)).ceil
+
+        # Configure the data
+        # + If first page, put in data for nextPage tile
+        # + If last page, put in data for prevPage tile
+        # + If middle page, put in data for both tiles
+        data = []
+        if params["page"] != "1"
+            if params["page"].to_i - 1 == 1 && params["last_page"] != "2"
+                limit = params['limit'].to_i + 1
+            elsif params["page"].to_i == params["last_page"]
+                limit = params['limit'].to_i - 1
+            else
+                limit = params['limit']
+            end
+            prev_page = {}
+            prev_page["title"] = "Previous"
+            prev_page["id"] = ""
+            prev_page["icon"] = "/prevPage.gif"
+            prev_page["layout"] = "/twitch/getTopGames?limit=#{limit}&count=#{params['count']}&page=#{params['page'].to_i-1}"
+            prev_page["layout"] += "&offset=#{params['offset'].to_i-params['limit'].to_i}&last_page=#{last_page}" # adding in the last_page variable as a temporary solution
+            data.push(prev_page) 
+        end
+        for game in top_games
+            puts game
+            sub_data = {}
+            sub_data["title"] = game["game"]["name"]
+            sub_data["id"] = game["game"]["_id"]
+            sub_data["icon"] = game["game"]["box"]["large"]
+            sub_data["layout"] = "games-list"
+            data.push(sub_data)
+        end
+        if params["page"] != last_page.to_s
+            if params["page"].to_i + 1 == last_page && params["page"] != "1"
+                limit = params['limit'].to_i + 1
+            elsif params["page"] == "1"
+                limit = params['limit'].to_i - 1
+            else
+                limit = params['limit']
+            end
+            next_page = {}
+            next_page["title"] = "Next"
+            next_page["id"] = ""
+            next_page["icon"] = "/nextPage.gif"
+            next_page["layout"] = "/twitch/getTopGames?limit=#{limit}&count=#{params['count']}&page=#{params['page'].to_i+1}" 
+            next_page["layout"] += "&offset=#{params['offset'].to_i+params['limit'].to_i}&last_page=#{last_page}" # adding in the last_page variable as a temporary solution
+            data.push(next_page)
+        end
+
+        return data.to_json
+    end
+
+    def getTopVideos(params)
+        #== Params
+        # *id*: the 
+        # *limit*: the limit for the amount of tiles that will be on the page
+        # offset: the offset so that the results are started at the correct page
+        # count: the total count of the resource
+        # page: 1,2,3,4,...
+
+        req_params = {}
+        req_params["limit"] = params["limit"]
+        req_params["offset"] = params["offset"]
+        req_params["broadcasts"] = "true"
+
+        resp = RestClient.get("https://api.twitch.tv/kraken/videos/top",:params => req_params, :authorization => "OAuth #{@access_token}")
+        json = JSON.parse(resp)
+
+        videos = json['videos']
+
+        # + If the the total count wasn't passed, find it from the response. 
+        # + Find the ideal pageSize using the getPageSize method. 
+        # + pageSize != limit, reset the limit to pageSize and call the method again
+        if params["count"].to_s.empty?
+            params["count"] = "1000000"
+            params["last_page"] = "1000"
+            params["page"] = "1"
+            params["limit"] = params["limit"].to_i - 1
+            params["offset"] = 0
+            videos = videos.slice!(0,params["limit"].to_i)
+        end
+
+        # Find the total amount of pages possible based on count and limit. Also
+        # caulculate offset
+        last_page = params["last_page"].to_i
+        offset = (params["limit"].to_i * (params["page"].to_i - 1)) + (1 * ((params["page"].to_i-1)/1000)).ceil
+
+        # Configure the data
+        # + If first page, put in data for nextPage tile
+        # + If last page, put in data for prevPage tile
+        # + If middle page, put in data for both tiles
+        data = []
+        if params["page"] != "1"
+            if params["page"].to_i - 1 == 1 && params["last_page"] != "2"
+                limit = params['limit'].to_i + 1
+            elsif params["page"].to_i == params["last_page"]
+                limit = params['limit'].to_i - 1
+            else
+                limit = params['limit']
+            end
+            prev_page = {}
+            prev_page["title"] = "Previous"
+            prev_page["id"] = ""
+            prev_page["icon"] = "/prevPage.gif"
+            prev_page["layout"] = "/twitch/getTopVideos?limit=#{limit}&count=#{params['count']}&page=#{params['page'].to_i-1}"
+            prev_page["layout"] += "&offset=#{params['offset'].to_i-params['limit'].to_i}&last_page=#{last_page}" # adding in the last_page variable as a temporary solution
+            data.push(prev_page) 
+        end
+        for video in videos
+            sub_data = {}
+            sub_data["title"] = video["title"]
+            sub_data["id"] = video["url"]
+            sub_data["icon"] = video["preview"]
+            sub_data["layout"] = ""
+            data.push(sub_data)
+        end
+        if params["page"] != last_page.to_s
+            if params["page"].to_i + 1 == last_page && params["page"] != "1"
+                limit = params['limit'].to_i + 1
+            elsif params["page"] == "1"
+                limit = params['limit'].to_i - 1
+            else
+                limit = params['limit']
+            end
+            next_page = {}
+            next_page["title"] = "Next"
+            next_page["id"] = ""
+            next_page["icon"] = "/nextPage.gif"
+            next_page["layout"] = "/twitch/getTopVideos?limit=#{limit}&count=#{params['count']}&page=#{params['page'].to_i+1}" 
+            next_page["layout"] += "&offset=#{params['offset'].to_i+params['limit'].to_i}&last_page=#{last_page}" # adding in the last_page variable as a temporary solution
+            data.push(next_page)
+        end
+
+        return data.to_json
+    end
+
     def getResourceUrl(id)
-        return "https://www.youtube.com/watch?v=" + id
+        return id
     end
 end
